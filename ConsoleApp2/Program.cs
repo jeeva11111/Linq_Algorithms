14b07a2f38f5a38e5f6b1f01da6882493e12bbb6
CIPL1473 <CIPL1473@CIPL1473DOTNET>
2023-12-18 10:58:43 +0530

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,18 @@
+﻿
+namespace MyCompiler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var objects = new[] { "Pen", "Pencil", "Scale", "Box" };
+
+            var store = from obj in objects select obj.Contains('a');
+            Console.WriteLine(string.Join (",", store));
+
+        }
+    }
+}
+
+
---
+++ ConsoleApp2/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Student
+    {
+
+        public int Id { get; set; }
+        public int Id2 { get; set; }
+        public string? Name { get; set; }
+    }
+}
